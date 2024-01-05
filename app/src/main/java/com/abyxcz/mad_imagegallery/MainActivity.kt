8cdacde456880de15305d85ddccd039b9000ce35
TM <tjmcard@gmail.com>
2024-01-05 10:47:54 -0800

    Fragment Interop in Compose

--- app/src/main/java/com/abyxcz/mad_imagegallery/MainActivity.kt
+++ app/src/main/java/com/abyxcz/mad_imagegallery/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.abyxcz.mad_imagegallery
 
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -10,9 +15,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import com.abyxcz.mad_imagegallery.databinding.Camera2ImageCaptureBinding
+import com.abyxcz.mad_imagegallery.databinding.MyFragmentLayoutBinding
 import com.abyxcz.mad_imagegallery.ui.theme.MADImageGalleryTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,9 +34,32 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    FragmentContainerView()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun FragmentContainerView() {
+    /*AndroidView(factory = { context ->
+        // This is the FrameLayout in which the Fragment will be placed
+        FrameLayout(context).apply {
+            // Generate a unique ID for this view to identify it during Fragment transactions
+            id = View.generateViewId()
+            layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+
+            // Perform the Fragment transaction
+            supportFragmentManager.commit {
+                replace(id, Camera2ImageCapture())
+            }
+        }
+    })*/
+
+    AndroidViewBinding(MyFragmentLayoutBinding::inflate) {
+        val myFragment = fragmentContainerView.getFragment<Camera2ImageCapture>()
+        // ...
     }
 }
 
